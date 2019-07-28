0f8b810ffa33310cec28926a0c88ec8d373b4189
david-cortes <david.cortes.rivera@gmail.com>
2019-07-28 12:16:53 +0300

    fix issued with R's WAP version

--- src/rwrapper.c
+++ src/rwrapper.c
@@ -13,25 +13,29 @@ SEXP r_calc_v(SEXP C, SEXP V, SEXP nrow, SEXP ncol)
 	size_t n_col = (size_t) INTEGER(ncol)[0];
 	double *V_arr = (double*) malloc(sizeof(double) * n_row * n_col);
 	double *C_arr = (double*) malloc(sizeof(double) * n_row * n_col);
+	double *C_ptr = (double*) REAL(C)
+	double *V_ptr = (double*) REAL(V)
 	if (V_arr == NULL || C_arr == NULL) {
 		REprintf("Could not allocate enough memory for the procedure");
-		EXIT_FAILURE;
+		return EXIT_FAILURE;
 	}
 	size_t ntot = n_row * n_col;
 	for (size_t i = 0; i < ntot; i++) {
-		C_arr[i] = REAL(C)[i];
+		C_arr[i] = C_ptr[i];
 	}
 
-	int failed = calculate_V(C_arr, V_arr, n_row, n_col, 1);
-	free(C_arr);
-	free(V_arr);
+	int failed = calculate_V(C_arr, V_arr, n_row, n_col, 1)
 	if (failed) {
 		REprintf("Could not allocate enough memory for the procedure");
-		EXIT_FAILURE;
+		free(C_arr);
+		free(V_arr);
+		return EXIT_FAILURE;
 	}
 
 	for (size_t i = 0; i < ntot; i++) {
-		REAL(V)[i] = V_arr[i];
+		V_ptr[i] = V_arr[i];
 	}
-	return 0;
+	free(C_arr);
+	free(V_arr);
+	return EXIT_SUCCESS;
 }

eef8735a2de50ba59ea2d31cba6ea1b97922deb8
david-cortes <david.cortes.rivera@gmail.com>
2024-02-24 11:22:48 +0100

    RTD attempt 3

--- docs/conf.py
+++ docs/conf.py
@@ -32,10 +32,16 @@ import scipy.stats
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
+]
 napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_ivar = True
+html_theme = "sphinx_rtd_theme"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
